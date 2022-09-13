f955eb234c52787eb555270392e00436a2ad9762
plavy <86317719+plavy@users.noreply.github.com>
2022-09-13 17:53:13 +0200

    add custom invite link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -114,7 +114,7 @@ const config = {
             items: [
               {
                 label: 'Discord',
-                href: 'https://discord.gg/8qCpr2aSsw',
+                href: 'https://discord.gg/hyperbiome',
               },
             ],
           },
