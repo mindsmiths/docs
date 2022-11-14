22f6f9b726fd5be18b7d3b651140c42da46bc362
Juraj Malenica <juraj@mindsmiths.com>
2022-11-14 15:03:26 +0100

    fixes main button text

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/Tutorials/intro">
+            to="/docs/src/Tutorials/intro">
             Tutorials
           </Link>
         </div>
